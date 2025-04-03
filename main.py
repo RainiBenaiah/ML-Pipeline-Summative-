71adc4b0671456c06db2d712d0fcdc30622e0b71
RainiBenaiah <127124519+RainiBenaiah@users.noreply.github.com>
2025-04-03 16:19:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def retrain():
         y = df["NObeyesdad"].astype(int)
 
         # Split into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
         # Train new model
         new_model = RandomForestClassifier(
